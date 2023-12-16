61ae4e6e07c66cbd0b9175c3402c9d85e58a9843
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2023-12-16 11:10:53 -0500

    Mi primer DART

---
+++ bin/hola_nuevamente.dart
@@ -0,0 +1,5 @@
+import 'package:hola_nuevamente/hola_nuevamente.dart' as hola_nuevamente;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hola_nuevamente.calculate()}!');
+}
---
+++ test/hola_nuevamente_test.dart
@@ -0,0 +1,8 @@
+import 'package:hola_nuevamente/hola_nuevamente.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
